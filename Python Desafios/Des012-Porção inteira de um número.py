67ad3e1caef04959534cf1231302fce1d6e0e2b1
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-01-22 17:35:49 -0300

    Porção inteira

---
+++ Python Desafios/Des012-Porção inteira de um número.py
@@ -0,0 +1,7 @@
+import math
+print('Desafio 012')
+print('Mostrar o número em porção inteira')
+
+num = float(input('Digite um valor: '))
+inteira = math.trunc(num)
+print('A parte inteira de {} é {}'.format(num, inteira))
