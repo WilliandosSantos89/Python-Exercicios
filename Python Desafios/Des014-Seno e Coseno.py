846cc3550ef8511a0369b8ab74ece3100b1b8082
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-07 20:52:49 -0300

    Criação do código

--- Python Desafios/Des014-Seno e Coseno.py
+++ Python Desafios/Des014-Seno e Coseno.py
@@ -1,3 +1,19 @@
 print('>>> Desafio 014 <<<')
 print('>>> Seno e Coseno <<<')
 
+import math
+
+ângulo = float(input('Dgite um ângulo: '))
+
+seno = math.sin(math.radians(ângulo))
+print('O ângulo de {} tem o SENO de {:.2}.'.format(ângulo, seno))
+
+coseno = math.cos(math.radians(ângulo))
+print('O ângulo de {} tem o COSENO de {:.2f}.'.format(ângulo, coseno))
+
+tangente = math.tan(math.radians(ângulo))
+print('O ângulo de {} tem a TANGENTE de {:.2f}.'.format(ângulo, tangente))
+
+
+
+
