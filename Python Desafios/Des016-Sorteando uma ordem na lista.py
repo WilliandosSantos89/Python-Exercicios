dbf8e88344688cb0a665fa4509c855aade6a3a44
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-08 11:48:45 -0300

    criação do código

--- Python Desafios/Des016-Sorteando uma ordem na lista.py
+++ Python Desafios/Des016-Sorteando uma ordem na lista.py
@@ -1,2 +1,17 @@
 print('>>> Desafio 016 <<<')
 print('>>> Sorteando uma ordem na lista <<<')
+
+import random
+
+aluno1 = str(input('Primeiro Aluno: '))
+aluno2 = str(input('Segundo Aluno: '))
+aluno3 = str(input('Terceiro Aluno: '))
+aluno4 = str(input('Quarto Aluno: '))
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+random.shuffle(lista)
+
+print('A ordem de apresentação será:')
+print(lista)
+
