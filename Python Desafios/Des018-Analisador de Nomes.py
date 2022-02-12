d892e4546d4e3d308ce85a364e903df09bd094d1
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-12 11:03:36 -0300

    Criação do arquivos separador de dígitos

--- Python Desafios/Des018-Analisador de Nomes.py
+++ Python Desafios/Des018-Analisador de Nomes.py
@@ -1,5 +1,16 @@
 print('>>> Desafio 18 <<<')
 print('>>> Analisador de Nomes <<<')
 
-nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: ')).strip()
+separa = nome.split()
+
+print('Verifique suas informações')
+
+print('Seu nome completo é {}'.format(nome))
+print('Seu nome em letras MAIÚSCULAS: {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas: {}'.format(nome.lower()))
+print('Seu nome tem {} letras.'. format(len(nome)-nome.count(' ')))
+#print(separa)
+print('Seu primeiro nome é {} e tem {} letras.'.format(separa[0], len(separa[0])))
+
 
