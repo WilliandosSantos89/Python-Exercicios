2f3b325ff20ce2539b712ed7107e853b18956e1b
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-15 15:53:38 -0300

    criação do arquivo procurando uma streang dentro de outra

---
+++ Python Desafios/Des021-Procurando um streang dentro de outra.py
@@ -0,0 +1,5 @@
+print('>>> Desafio 021 <<<')
+print('>>> Procurando uma streang dentro de outra <<<')
+
+nome = str(input('Digite seu nome completo: ')).strip()
+
---
+++ Python Desafios/Des20-Analisando a primeira letra.py
@@ -0,0 +1,9 @@
+print('>>> Desafio 020 <<<')
+print('>>> Analisando a primeira letra <<<')
+
+
+
+cidade = str(input('Digite o nome da sua cidade:')).strip()
+
+print('Verificando se a cidade contém a palvra "Santo"...')
+print(cidade[:5].upper() == 'SANTO')
