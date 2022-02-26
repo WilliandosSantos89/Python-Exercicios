dca14a21c5513d0861955aedb98d7f90948c0d88
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-26 11:10:45 -0300

    Criação do Desafio 029-Menor e Maior Valores

--- Python Desafios/Des028-Ano Bissexto.py
+++ Python Desafios/Des028-Ano Bissexto.py
@@ -1,8 +1,12 @@
 print('>>> Desafio 028 <<<')
 print('>>> Ano Bissexto <<<')
 
+from datetime import date
+
 ano = int(input('Qual ano você quer analisar? '))
 
+if ano == 0:
+    ano = date.today().year
 if ano % 4 == 0 and ano % 100!= 0 or ano % 400 == 0:
     print('O ano de {} é Bissexto!'.format(ano))
 else:
