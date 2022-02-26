26cafb4eb61207fb64923800432eefae04236b23
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-26 14:38:55 -0300

    Criação do Desafio 030-Aumentos Múltiplos

---
+++ Python Desafios/Des029-Menor e Maior Valores.py
@@ -0,0 +1,28 @@
+print('>>> Desafio 029 <<<')
+print('>>> Maior  e Menor Valores <<<')
+
+n1 = int(input('Digite o primeiro valor:'))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+#Testando o menor
+menor = n1
+if n2<n1 and n2<n3:
+    menor = n2
+if n3<n1 and n3<n2:
+    menor = n3
+
+#Testando o maior
+maior = n1
+if n2>n1 and n2>n3:
+    maior = n2
+if n3>n1 and n3>n2:
+    maior = n3
+
+
+print('O menor valor digitado foi {}.'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
+
+
+
+
