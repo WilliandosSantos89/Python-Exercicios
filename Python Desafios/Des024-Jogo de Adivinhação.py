7cd450c96e1d655098d55f54502cd79cc6b8ad02
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-02-26 09:17:45 -0300

    Criação do Desafio 025-Radar Eletrônico

--- Python Desafios/Des024-Jogo de Adivinhação.py
+++ Python Desafios/Des024-Jogo de Adivinhação.py
@@ -1,5 +1,34 @@
 print('>>> Desafio 024 <<<')
 print('>>> Jogo da Adivinhação <<<')
 
-import random
+from random import randint
+from time import sleep
+
+
+computador = randint(0,5) #PC pensando
+
+print('Vou pensar em um número entre 0 e 5, tente adivinhar...')
+
+print('-=-' * 20)
+
+print('-=-' * 20)
+
+jogador = int(input('Em que número eu pensei?')) #Jogador tentando adivinha
+
+print('Processando...')
+sleep(3)
+
+#Condições
+
+if jogador == computador:
+    print('Parabéns, você acertou!')
+else:
+    print('Que pena, você errou! Eu pensei no número {} e não no número {}'.format(computador, jogador))
+
+
+
+
+
+
+
 
