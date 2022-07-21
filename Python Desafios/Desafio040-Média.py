1bd37ddae9a57bf25393d1b5e36110359d4988ce
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2022-07-21 20:57:15 -0300

    Desafio 40 - média

---
+++ Python Desafios/Desafio040-Média.py
@@ -0,0 +1,14 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media < 5.0:
+  print("Reprovado!")
+
+elif media > 5 < 6.9:
+  print("Recuperação!")
+
+
+elif media > 7:
+  print("Aprovado!")
