e20e3d65283804901b780019487fb18886492e80
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2022-07-22 17:03:57 -0300

    implementação do sexo

--- Python Desafios/Des039-AlistamentoMilitar.py
+++ Python Desafios/Des039-AlistamentoMilitar.py
@@ -4,25 +4,36 @@ anoAtual = date.today().year
 
 anoNascimento = int(input("Digite seu ano de nascimento com 4 dígitos: "))
 
-idade = anoAtual - anoNascimento
+print('''Informe seu sexo:
+[ 1 ] Masculino
+[ 2 ] Feminino''')
 
-print("Quem nasceu em {}, tem {} anos em {}.".format(anoNascimento, idade, anoAtual))
+sexo = int(input("Informe seu sexo: "))
 
-if idade == 18:
-  print("Você tem {} anos, ja tem que se alistar!". format(idade))
+if sexo == 2:
+  print("Você não precisa se alistar por ser mulher!")
 
-elif idade < 18:
-  saldo = 18 - idade
-  print("Ainda faltam {} anos para você se alistar.".format(saldo))
+else:
 
-  ano = anoAtual + saldo
-  print("Seu alistamento será em {}".format(ano))
-  
+  idade = anoAtual - anoNascimento
 
-elif anoNascimento > idade:
-  saldo = idade - 18
-  print("Já passaram {} anos do prazo do seu alistamento.".format(saldo))
+  print("Quem nasceu em {}, tem {} anos em {}.".format(anoNascimento, idade, anoAtual))
 
-  ano = anoAtual - saldo
-  print("Você deveria ter se alistado em {}.".format(ano))
-  
+  if idade == 18:
+    print("Você tem {} anos, ja tem que se alistar!". format(idade))
+
+  elif idade < 18:
+    saldo = 18 - idade
+    print("Ainda faltam {} anos para você se alistar.".format(saldo))
+
+    ano = anoAtual + saldo
+    print("Seu alistamento será em {}".format(ano))
+    
+
+  elif anoNascimento > idade:
+    saldo = idade - 18
+    print("Já passaram {} anos do prazo do seu alistamento.".format(saldo))
+
+    ano = anoAtual - saldo
+    print("Você deveria ter se alistado em {}.".format(ano))
+    
