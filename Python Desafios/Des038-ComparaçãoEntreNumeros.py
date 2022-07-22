adb731dd35f8d1e0f28e510be26b0e077c3cd43e
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2022-07-22 16:48:25 -0300

    desafios 38 e 39

---
+++ Python Desafios/Des038-ComparaçãoEntreNumeros.py
@@ -0,0 +1,11 @@
+num1 = int(input("Escreva um número: "))
+num2 = int(input("Escreva outro número: "))
+
+if num1 > num2:
+  print("O primeiro valor é maior que o segundo!")
+
+elif num2 > num1:
+  print("O segundo valor é maior que o primeiro!")
+
+else:
+  print("Os 2 números são iguais.")
---
+++ Python Desafios/Des039-AlistamentoMilitar.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+anoAtual = date.today().year
+
+anoNascimento = int(input("Digite seu ano de nascimento com 4 dígitos: "))
+
+idade = anoAtual - anoNascimento
+
+print("Quem nasceu em {}, tem {} anos em {}.".format(anoNascimento, idade, anoAtual))
+
+if idade == 18:
+  print("Você tem {} anos, ja tem que se alistar!". format(idade))
+
+elif idade < 18:
+  saldo = 18 - idade
+  print("Ainda faltam {} anos para você se alistar.".format(saldo))
+
+  ano = anoAtual + saldo
+  print("Seu alistamento será em {}".format(ano))
+  
+
+elif anoNascimento > idade:
+  saldo = idade - 18
+  print("Já passaram {} anos do prazo do seu alistamento.".format(saldo))
+
+  ano = anoAtual - saldo
+  print("Você deveria ter se alistado em {}.".format(ano))
+  
