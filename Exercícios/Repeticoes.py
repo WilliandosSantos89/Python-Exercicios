856b2352c8207b65a21aafec4fe59aa924126969
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2022-08-05 15:15:08 -0300

    alterações

--- Exercícios/Repeticoes.py
+++ Exercícios/Repeticoes.py
@@ -3,7 +3,20 @@ from click import pass_obj
 
 print(">>> Laços <<<")
 
-c = int
+#for c in range(6,0, -1): #de 6 a 0 de 1 em 1
+#  print(c)
+#print("fim")
 
-for c in range.(1,10:):
-  p
+#for c in range(2,12, 2): de 2 a 10 contando de 2 em 2
+#  print(c)
+
+#n = int(input("Digite um número: "))
+#for c in range (0, n-1):
+#  print(c)
+
+início = int(input("Início: "))
+fim = int(input("fim: "))
+passo = int(input("passo: "))
+for c in range (início, fim+1, passo):
+    print(c)
+print("fim")
