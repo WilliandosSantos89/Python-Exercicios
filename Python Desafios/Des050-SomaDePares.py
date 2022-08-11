3891559af5f5d796e8e097da648d251de4b37e74
williandossantos89 <willian.dos.santos89@gmail.com>
2022-08-11 11:51:54 -0300

    desafio 50

--- Python Desafios/Des050-SomaDePares.py
+++ Python Desafios/Des050-SomaDePares.py
@@ -5,5 +5,6 @@ for c in range (1,7):
     num = int(input('Digite o {}º número: '.format(c)))
     if num % 2 == 0:
         soma += num
+        contador += 1
         
-print('A soma dos números pares é {}'.format(soma))
+print('A soma do(s) {} número(s) pare(s) é {}'.format(contador, soma))
